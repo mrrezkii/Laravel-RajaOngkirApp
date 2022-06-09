7fe6c4186118b7a09a8573fcec12763fe9464768
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-09 22:19:16 +0700

    Add resource files

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Raja Ongkir - Dashboard</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    @yield('custom-head')
+</head>
+
+<body id="page-top">
+<div id="wrapper">
+    @include('partials.navbar')
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </nav>
+            <div class="container-fluid">
+                @yield('container')
+            </div>
+        </div>
+        @include('partials.footer')
+    </div>
+</div>
+<script src="{{ url('/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ url('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ url('/js/sb-admin-2.min.js"') }}"></script>
+@yield('custom-script')
+</body>
+
+</html>
