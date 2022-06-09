53f24caa0e78953ab0eae7891bda8fb5ad9f190a
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-09 22:42:19 +0700

    Remove unused resource

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,11 +8,11 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Raja Ongkir - Dashboard</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{url('css/sb-admin-2.min.css')}}" rel="stylesheet">
     @yield('custom-head')
 </head>
 
