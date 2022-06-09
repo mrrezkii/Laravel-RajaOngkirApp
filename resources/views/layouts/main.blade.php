0fb0f47fef4d961eb47edaafceccefa975967e72
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-06-09 23:25:49 +0700

    Create dashboard view

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Raja Ongkir - Dashboard</title>
+    <title>Raja Ongkir - {{ $title }}</title>
     <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
@@ -35,7 +35,7 @@
 </div>
 <script src="{{ url('/vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ url('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ url('/js/sb-admin-2.min.js"') }}"></script>
+<script src="{{ url('/js/sb-admin-2.min.js') }}"></script>
 @yield('custom-script')
 </body>
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,12 +1,12 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('dashboard') }}">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
         <div class="sidebar-brand-text mx-3">Raja Ongkir</div>
     </a>
     <hr class="sidebar-divider my-0">
-    <li class="nav-item active">
+    <li class="nav-item {{ ($active === "dashboard") ? 'active' : '' }}">
         <a class="nav-link" href="index.html">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
@@ -15,13 +15,13 @@
     <div class="sidebar-heading">
         Features
     </div>
-    <li class="nav-item">
+    <li class="nav-item {{ ($active === "cost") ? 'active' : '' }}">
         <a class="nav-link collapsed" href="#">
             <i class="fas fa-truck-pickup"></i>
             <span>Cost</span>
         </a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item {{ ($active === "waybill") ? 'active' : '' }}">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
            aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-search-location pl-1"></i>
@@ -32,7 +32,6 @@
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Waybill</h6>
                 <a class="collapse-item" href="#">JNE</a>
-                <a class="collapse-item" href="#">Other</a>
             </div>
         </div>
     </li>
